f3035750773cf9d3ec3076c4a3020d56ae4afc62
Justin Ng <justinng@gmail.com>
2015-04-26 01:41:20 -0400

    More stuff!

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+// var http = require('http')
+// var port = process.env.PORT || 1337;
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World\n');
+// }).listen(port);
+
+var express = require('express');
+
+var server = express();
+server.use(express.static('/'));
+var port = 1337;
+
+server.listen(port, function(){
+    console.log('server Listening on port ' + port);
+});
